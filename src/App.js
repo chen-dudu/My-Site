4adc3eaef65f4f89ae0a9adacbc8969c2c7b5586
unknown <43197141+chen-dudu@users.noreply.github.com>
2020-11-30 18:12:45 +0800

    added new routes for contact page and home, both en version and cn version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
+import Home_en from "./pages-en/Home";
+import Home_cn from "./pages-cn/Home";
+import Contact_en from "./pages-en/Contact";
+import Contact_cn from "./pages-cn/Contact";
 import NotFound_en from "./pages-en/NotFound";
 import NotFound_cn from "./pages-cn/NotFound";
+
 
 function App() {
   return (
     <Router>
         <Switch>
             {/*<Route />*/}
+
+            {/* english pages */}
+            <Route exact path = "/" component={Home_en}/>
+            <Route exact path = "/Contact" component={Contact_en}/>
+
+
+            {/* chinese pages */}
+            <Route exact path = "/cn/" component={Home_cn}/>
+            <Route exact path = "/cn/Contact" component={Contact_cn}/>
+            {/*<Route exact path = "/cn/contact" component={Contact_cn}/>*/}
+
             {/* 404 page in chinese */}
-            <Route exact path = "/a" component={NotFound_cn} />
+            {/*<Route exact path = "/a" component={NotFound_cn} />*/}
+
+            {/* 404 page in english */}
             <Route component={NotFound_en}/>
         </Switch>
     </Router>
