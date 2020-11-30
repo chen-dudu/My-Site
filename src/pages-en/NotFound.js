4a0035362058625ded7af729c1b9a4e2c243b53a
unknown <43197141+chen-dudu@users.noreply.github.com>
2020-11-30 14:41:28 +0800

    finished implementing 404 page in english

---
+++ src/pages-en/NotFound.js
@@ -0,0 +1,32 @@
+import React from "react";
+import 'antd/dist/antd.css';
+import { Result, Button } from 'antd';
+
+class NotFound extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        this.props.history.push("/");
+    }
+
+    render() {
+        return (
+            <Result
+            status = "404"
+            title = "Page Not Found"
+            subTitle = "Sorry, the page you are requesting does not exist."
+            style = {{paddingTop: 80}}
+            extra = { <Button type = "primary" onClick = {this.handleClick} >
+                        Back to Homepage
+                      </Button> }
+            />
+
+        );
+    }
+}
+
+export default NotFound;
