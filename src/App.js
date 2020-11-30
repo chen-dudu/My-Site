de4b9abcf988141ffc24dfe5274c0adf79fce33f
unknown <43197141+chen-dudu@users.noreply.github.com>
2020-11-30 14:42:03 +0800

    added route for 404 pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import NotFound_en from "./pages-en/NotFound";
+import NotFound_cn from "./pages-cn/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/*Edit <code>src/App.js</code> and save to reload.*/}
-          Edit src/App.js and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+            {/*<Route />*/}
+            {/* 404 page in chinese */}
+            <Route exact path = "/a" component={NotFound_cn} />
+            <Route component={NotFound_en}/>
+        </Switch>
+    </Router>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       {/*Edit <code>src/App.js</code> and save to reload.*/}
+    //       Edit src/App.js and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
