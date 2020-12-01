f2e5843f432c16aea45da1b19be2d99cedffd8b7
unknown <43197141+chen-dudu@users.noreply.github.com>
2020-12-01 09:38:42 +0800

    added routes for study page, both cn and en versions

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Home_en from "./pages-en/Home";
-import Home_cn from "./pages-cn/Home";
 import Contact_en from "./pages-en/Contact";
+import Study_en from "./pages-en/Study";
+
+import Home_cn from "./pages-cn/Home";
 import Contact_cn from "./pages-cn/Contact";
+import Study_cn from "./pages-cn/Study";
+
 import NotFound_en from "./pages-en/NotFound";
 import NotFound_cn from "./pages-cn/NotFound";
 
@@ -18,12 +22,13 @@ function App() {
             {/* english pages */}
             <Route exact = {true} path = "/my-site/" component={Home_en}/>
             <Route exact = {true} path = "/my-site/Contact" component={Contact_en}/>
+            <Route exact = {true} path = "/my-site/Study" component={Study_en}/>
 
 
             {/* chinese pages */}
             <Route exact = {true} path = "/my-site/cn/" component={Home_cn}/>
             <Route exact = {true} path = "/my-site/cn/Contact" component={Contact_cn}/>
-            {/*<Route exact path = "/cn/contact" component={Contact_cn}/>*/}
+            <Route exact = {true} path = "/my-site/cn/Study" component={Study_cn}/>
 
             {/* 404 page in chinese */}
             {/*<Route exact path = "/a" component={NotFound_cn} />*/}
