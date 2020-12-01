361955d6aeb3e4399cbf0f0baf4753d99bd956f3
unknown <43197141+chen-dudu@users.noreply.github.com>
2020-12-01 09:39:17 +0800

    finished implementing top nav bar for study page, cn version

---
+++ src/pages-cn/Study.js
@@ -0,0 +1,51 @@
+import React from "react";
+import 'antd/dist/antd.css';
+import {Menu, Button} from 'antd';
+
+class Study extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClickButton = this.handleClickButton.bind(this);
+        this.handleClickItemHome = this.handleClickItemHome.bind(this);
+        this.handleClickItemStudy = this.handleClickItemStudy.bind(this);
+        this.handleClickItemContact = this.handleClickItemContact.bind(this);
+    }
+
+    handleClickButton() {
+        this.props.history.push("/my-site/Study");
+    }
+
+    handleClickItemHome(e) {
+        this.props.history.push("/my-site/cn/");
+    }
+
+    // not being used currently, left here for future
+    handleClickItemStudy(e) {
+        // this.props.history.push("/my-site/cn/Study");
+    }
+
+    handleClickItemContact(e) {
+        this.props.history.push("/my-site/cn/Contact");
+    }
+
+    render() {
+        return (
+            <div>
+                <Menu theme={"light"} mode={"horizontal"} defaultSelectedKeys={['2']} style = {{ paddingRight: 50}}>
+                    <h1 style={{position: "relative", display: "inline-block", height: 1, width: "8%", paddingLeft: 20}}>
+                        欢迎
+                    </h1>
+                    <Menu.Item key = "1" style={{marginLeft: 1080}} onClick={this.handleClickItemHome}>主页</Menu.Item>
+                    <Menu.Item key = "2" onClick={this.handleClickItemStudy}>学习</Menu.Item>
+                    <Menu.Item key = "3" onClick={this.handleClickItemContact}>联系</Menu.Item>
+                    <Button style = {{marginLeft: 25}} onClick = {this.handleClickButton}>
+                        English
+                    </Button>
+                </Menu>
+            </div>
+        );
+    }
+}
+
+export default Study;
