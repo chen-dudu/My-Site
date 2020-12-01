2079a90d9da786c855647ae71429bb817f44a431
unknown <43197141+chen-dudu@users.noreply.github.com>
2020-12-01 10:38:29 +0800

    added file level comment for DB classes

--- src/DB/DB-cn.js
+++ src/DB/DB-cn.js
@@ -1,3 +1,6 @@
+/**
+ * @file this file contains all the data in Chinese, acting as DB for the system
+ */
 
 const DB = {
     phone: "000",
--- src/DB/DB-en.js
+++ src/DB/DB-en.js
@@ -1,3 +1,6 @@
+/**
+ * @file this file contains all the data in English, acting as the DB for the system
+ */
 
 const DB = {
     phone: "000",
